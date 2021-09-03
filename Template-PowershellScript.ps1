f9d3c5de97ed702b74d913c4bc31b16a4c20c65a
smoebot <kambah@gmail.com>
2021-09-03 12:42:54 +1000

    Create Template-PowershellScript.ps1

---
+++ Template-PowershellScript.ps1
@@ -0,0 +1,66 @@
+#requires -version 5
+<#
+.SYNOPSIS
+  <Overview of script>
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER <Parameter_Name>
+  <Brief description of parameter input required. Repeat this attribute if required>
+.PARAMETER Name2
+  Descripe the parameter
+.NOTES
+  Author: [author name]
+  v0.1 - (2020-03-05) Initial version
+  v0.2 - (2020-07-30) Added Something (contributor)
+.EXAMPLE
+  <Example explanation goes here>
+  
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  #Script parameters go here
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Any Global Declarations go here
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+Function <FunctionName> {
+  Param ()
+  Begin {
+    Write-Host '<description of what is going on>...'
+  }
+  Process {
+    Try {
+      <code goes here>
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed Successfully.'
+      Write-Host ' '
+    }
+  }
+}
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Script Execution goes here
